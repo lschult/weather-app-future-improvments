42a52a241f53366f37b0601abbd12e4132e23b2a
lschult <136801449+lschult@users.noreply.github.com>
2023-07-26 17:45:21 +0200

    feat: added improvement ideas from project

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * this function can be used to fetch all the records
+ */
+// async function getWeatherJournalData(data) {
+//   const response = await fetch(journalAPI, {
+//     mode: "cors", // no-cors, *cors, same-origin
+//     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+//     credentials: "same-origin", // include, *same-origin, omit
+//     headers: {
+//       "Content-Type": "application/json",
+//       // 'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//     body: JSON.stringify(data), // body data type must match "Content-Type" header
+//   });
+//   return response.json();
+// }
---
+++ server.js
@@ -0,0 +1,12 @@
+// app.get("/api/weather-journal/latest", (req, res) => {
+//   console.log("get latest value");
+
+//   const ids = Object.keys(projectData);
+//   const maxId = Math.max(...ids);
+
+//   res.json(projectData[maxId]);
+// });
+
+// function tempValid(tem) {
+//   // is it a number? range of numbers
+// }
